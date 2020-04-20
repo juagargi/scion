e48f57df35695039c5021d9c4a3dae304caea6a7
stygerma <stygerma@student.ethz.ch>
2020-04-20 13:59:14 +0200

    minor changes for demo

--- go/border/qos/queues/customQueue.go
+++ go/border/qos/queues/customQueue.go
@@ -160,6 +160,6 @@ func (pq *CustomPacketQueue) GetCongestionWarning() *CongestionWarning {
 	return &pq.pktQue.CongestionWarning
 }
 
-func (pq *CustomPacketQueue) GetTockenBucket() *TokenBucket {
+func (pq *CustomPacketQueue) GetTokenBucket() *TokenBucket {
 	return &pq.tb
 }
