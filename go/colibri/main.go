6a98c455e7e4ea83bf01ea40f49fc4c1a43aba09
Juan A. Garcia Pardo <juagargi@gmail.com>
2021-06-29 16:20:45 +0200

    WIP SvcCOL 2

---
+++ go/colibri/main.go
@@ -0,0 +1,292 @@
+// Copyright 2020 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"net"
+	"path/filepath"
+
+	"google.golang.org/grpc"
+
+	"github.com/scionproto/scion/go/cs/config"
+	admission "github.com/scionproto/scion/go/cs/reservation/segment/admission/stateless"
+	"github.com/scionproto/scion/go/cs/reservationstore"
+	"github.com/scionproto/scion/go/cs/segreq"
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/colibri/coliquic"
+	"github.com/scionproto/scion/go/lib/fatal"
+	"github.com/scionproto/scion/go/lib/infra/infraenv"
+	"github.com/scionproto/scion/go/lib/infra/messenger"
+	"github.com/scionproto/scion/go/lib/infra/modules/itopo"
+	segfetchergrpc "github.com/scionproto/scion/go/lib/infra/modules/segfetcher/grpc"
+	"github.com/scionproto/scion/go/lib/keyconf"
+	"github.com/scionproto/scion/go/lib/log"
+	"github.com/scionproto/scion/go/lib/pathdb"
+	"github.com/scionproto/scion/go/lib/revcache"
+	"github.com/scionproto/scion/go/lib/serrors"
+	"github.com/scionproto/scion/go/lib/snet"
+	"github.com/scionproto/scion/go/lib/topology"
+	"github.com/scionproto/scion/go/pkg/app/launcher"
+	"github.com/scionproto/scion/go/pkg/cs"
+	colgrpc "github.com/scionproto/scion/go/pkg/cs/colibri/grpc"
+	libgrpc "github.com/scionproto/scion/go/pkg/grpc"
+	colpb "github.com/scionproto/scion/go/pkg/proto/colibri"
+	"github.com/scionproto/scion/go/pkg/storage"
+	"github.com/scionproto/scion/go/pkg/trust"
+	"github.com/scionproto/scion/go/pkg/trust/compat"
+	trustgrpc "github.com/scionproto/scion/go/pkg/trust/grpc"
+)
+
+func main() {
+	var cfg config.Config
+	application := launcher.Application{
+		TOMLConfig: &cfg,
+		ShortName:  "SCION COLIBRI Service",
+		Main: func() error {
+			return realMain(&cfg)
+		},
+	}
+	application.Run()
+}
+
+func realMain(cfg *config.Config) error {
+
+	cfgObjs, err := setup(cfg)
+	if err != nil {
+		return err
+	}
+	if err := setupColibri(&cfg.Colibri, cfgObjs); err != nil {
+		return err
+	}
+	defer cfgObjs.closeFcn()
+
+	return nil
+}
+
+type cfgObjs struct {
+	masterKey keyconf.Master
+	revCache  revcache.RevCache
+	pathDB    pathdb.PathDB
+	trustDB   storage.TrustDB
+	nc        *infraenv.NetworkConfig
+	quicStack *infraenv.QUICStack
+	tcpStack  net.Listener
+	dialer    *libgrpc.QUICDialer
+	router    snet.Router
+	closeFcn  func() // defer a call to this function
+}
+
+func setup(cfg *config.Config) (*cfgObjs, error) {
+	topo, err := topology.FromJSONFile(cfg.General.Topology())
+	if err != nil {
+		return nil, serrors.WrapStr("loading topology", err)
+	}
+	itopo.Init(&itopo.Config{
+		ID:  cfg.General.ID,
+		Svc: topology.Colibri,
+	})
+	if err := itopo.Update(topo); err != nil {
+		return nil, serrors.WrapStr("unable to set initial static topology", err)
+	}
+	infraenv.InitInfraEnvironment(cfg.General.Topology())
+
+	cfgObjs, err := setupNetwork(cfg)
+	if err != nil {
+		return cfgObjs, serrors.WrapStr("setting network config", err)
+	}
+
+	cfgObjs.masterKey, err = keyconf.LoadMaster(filepath.Join(cfg.General.ConfigDir, "keys"))
+	if err != nil {
+		return nil, serrors.WrapStr("error getting master secret", err)
+	}
+
+	return cfgObjs, nil
+}
+
+func setupNetwork(cfg *config.Config) (*cfgObjs, error) {
+	revCache := storage.NewRevocationStorage()
+	pathDB, err := storage.NewPathStorage(cfg.PathDB)
+	if err != nil {
+		return nil, serrors.WrapStr("initializing path storage", err)
+	}
+	pathDB = pathdb.WithMetrics(string(storage.BackendSqlite), pathDB)
+
+	trustDB, err := storage.NewTrustStorage(cfg.TrustDB)
+	if err != nil {
+		return nil, serrors.WrapStr("initializing trust storage", err)
+	}
+
+	topo := itopo.Get()
+	nc := &infraenv.NetworkConfig{
+		IA:                    topo.IA(),
+		Public:                topo.PublicAddress(addr.SvcCOL, cfg.General.ID),
+		ReconnectToDispatcher: cfg.General.ReconnectToDispatcher,
+		QUIC: infraenv.QUIC{
+			Address: cfg.QUIC.Address,
+		},
+		SVCRouter: messenger.NewSVCRouter(itopo.Provider()),
+		SCMPHandler: snet.DefaultSCMPHandler{
+			RevocationHandler: cs.RevocationHandler{RevCache: revCache},
+		},
+	}
+	quicStack, err := nc.QUICStack()
+	if err != nil {
+		return nil, serrors.WrapStr("initializing QUIC stack", err)
+	}
+	tcpStack, err := nc.TCPStack()
+	if err != nil {
+		return nil, serrors.WrapStr("initializing TCP stack", err)
+	}
+
+	dialer := &libgrpc.QUICDialer{
+		Rewriter: nc.AddressRewriter(nil),
+		Dialer:   quicStack.Dialer,
+	}
+
+	cfgObjs := &cfgObjs{
+		revCache:  revCache,
+		pathDB:    pathDB,
+		trustDB:   trustDB,
+		nc:        nc,
+		quicStack: quicStack,
+		tcpStack:  tcpStack,
+		dialer:    dialer,
+		closeFcn: func() {
+			defer revCache.Close()
+			defer quicStack.RedirectCloser()
+			defer pathDB.Close()
+		},
+	}
+
+	return withRouter(cfg, cfgObjs)
+}
+
+func withRouter(cfg *config.Config, cfgObjs *cfgObjs) (*cfgObjs, error) {
+
+	topo := itopo.Get()
+	trustengineCache := cfg.TrustEngine.Cache.New()
+	inspector := trust.CachingInspector{
+		Inspector: trust.DBInspector{
+			DB: cfgObjs.trustDB,
+		},
+		// CacheHits:          cacheHits,
+		MaxCacheExpiration: cfg.TrustEngine.Cache.Expiration,
+		Cache:              trustengineCache,
+	}
+	provider := trust.FetchingProvider{
+		DB: cfgObjs.trustDB,
+		Fetcher: trustgrpc.Fetcher{
+			IA:     topo.IA(),
+			Dialer: cfgObjs.dialer,
+			// Requests: libmetrics.NewPromCounter(trustmetrics.RPC.Fetches),
+		},
+		Recurser: trust.ASLocalRecurser{IA: topo.IA()},
+	}
+	verifier := compat.Verifier{
+		Verifier: trust.Verifier{
+			Engine: provider,
+			// CacheHits:          cacheHits,
+			MaxCacheExpiration: cfg.TrustEngine.Cache.Expiration,
+			Cache:              trustengineCache,
+		},
+	}
+
+	fetcherCfg := segreq.FetcherConfig{
+		IA:            itopo.Get().IA(),
+		PathDB:        cfgObjs.pathDB,
+		RevCache:      cfgObjs.revCache,
+		QueryInterval: cfg.PS.QueryInterval.Duration,
+		RPC: &segfetchergrpc.Requester{
+			Dialer: cfgObjs.dialer,
+		},
+		Inspector:    inspector,
+		TopoProvider: itopo.Provider(),
+		Verifier:     verifier,
+	}
+
+	cfgObjs.router = segreq.NewRouter(fetcherCfg)
+	provider.Router = trust.AuthRouter{
+		ISD:    topo.IA().I,
+		DB:     cfgObjs.trustDB,
+		Router: cfgObjs.router,
+	}
+	return cfgObjs, nil
+}
+
+func setupColibri(cfg *config.ColibriConfig, cfgObjs *cfgObjs) error {
+	db, err := storage.NewColibriStorage(cfg.DB)
+	if err != nil {
+		return serrors.WrapStr("error initializing COLIBRI DB", err)
+	}
+
+	admitter := &admission.StatelessAdmission{
+		Caps:  cfg.Capacities,
+		Delta: cfg.Delta,
+	}
+	colDialer := &libgrpc.QUICDialer{
+		Rewriter: cfgObjs.nc.AddressRewriter(nil),
+		Dialer:   cfgObjs.quicStack.Dialer,
+	}
+
+	colibriStore, err := reservationstore.NewStore(itopo.Get(), cfgObjs.router, cfgObjs.nc.AddressRewriter(nil),
+		colDialer, db, admitter, cfgObjs.masterKey.Key0)
+	if err != nil {
+		return serrors.WrapStr("initializing colibri store", err)
+	}
+
+	colibriService := &colgrpc.ColibriService{
+		Store: colibriStore,
+	}
+	// colpb.RegisterColibriServer(quicServer, colibriService)
+	colServer := coliquic.NewGrpcServer(libgrpc.UnaryServerInterceptor())
+	tcpColServer := grpc.NewServer(libgrpc.UnaryServerInterceptor())
+	colpb.RegisterColibriServer(colServer, colibriService)
+	colpb.RegisterColibriServer(tcpColServer, colibriService)
+
+	// run inter and intra AS servers
+	topo := itopo.Get()
+	go func() {
+		defer log.HandlePanic()
+		lis, err := coliquic.ColibriListener(topo)
+		if err != nil {
+			fatal.Fatal(err)
+		}
+		log.Info("DELETEME %%%%%%%%% colibri grpc server listening", "addr", lis.Addr())
+		if err := colServer.Serve(lis); err != nil {
+			fatal.Fatal(err)
+		}
+	}()
+	go func() {
+		defer log.HandlePanic()
+		// TODO(juagargi) integrate TCP and QUIC with just one listener in coliquic.ColibriListener
+		publicAddr, err := topo.Anycast(addr.SvcCOL)
+		if err != nil {
+			fatal.Fatal(err)
+		}
+		tcpListener, err := net.ListenTCP("tcp", &net.TCPAddr{
+			IP:   publicAddr.IP,
+			Port: publicAddr.Port,
+			Zone: publicAddr.Zone,
+		})
+		if err != nil {
+			fatal.Fatal(err)
+		}
+		log.Info("DELETEME %%%%%%%%% colibri TCP grpc server listening", "tcp_addr", tcpListener.Addr())
+		if err := tcpColServer.Serve(tcpListener); err != nil {
+			fatal.Fatal(err)
+		}
+	}()
+	return nil
+}
--- go/lib/topology/interface.go
+++ go/lib/topology/interface.go
@@ -262,6 +262,8 @@ func (t *topologyS) topoAddress(svc addr.HostSVC, name string) *TopoAddr {
 		addresses = t.Topology.DS
 	case addr.SvcCS:
 		addresses = t.Topology.CS
+	case addr.SvcCOL:
+		addresses = t.Topology.CO
 	case addr.SvcSIG:
 		if len(t.Topology.SIG) == 0 {
 			break
@@ -329,7 +331,7 @@ func (t *topologyS) UnderlayAnycast(svc addr.HostSVC) (*net.UDPAddr, error) {
 
 func supportedSVC(svc addr.HostSVC) bool {
 	b := svc.Base()
-	return b == addr.SvcDS || b == addr.SvcCS || b == addr.SvcSIG
+	return b == addr.SvcDS || b == addr.SvcCS || b == addr.SvcCOL || b == addr.SvcSIG
 }
 
 func (t *topologyS) UnderlayMulticast(svc addr.HostSVC) ([]*net.UDPAddr, error) {
@@ -387,7 +389,7 @@ func toServiceType(svc addr.HostSVC) (ServiceType, error) {
 	case addr.SvcCS:
 		return Control, nil
 	case addr.SvcCOL:
-		return Control, nil
+		return Colibri, nil
 	case addr.SvcSIG:
 		return Gateway, nil
 	default:
@@ -422,6 +424,8 @@ func (t *topologyS) SVCNames(svc addr.HostSVC) ServiceNames {
 		m = t.Topology.DS
 	case addr.SvcCS:
 		m = t.Topology.CS
+	case addr.SvcCOL:
+		m = t.Topology.CO
 	case addr.SvcSIG:
 		if len(t.Topology.SIG) == 0 {
 			break
@@ -430,8 +434,6 @@ func (t *topologyS) SVCNames(svc addr.HostSVC) ServiceNames {
 		for k, v := range t.Topology.SIG {
 			m[k] = *v.CtrlAddr
 		}
-	case addr.SvcCOL:
-		m = t.Topology.CS
 	}
 
 	var names ServiceNames
