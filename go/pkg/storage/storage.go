6b0af265a6a30e88a72e636dd3f6c6380f589e9e
Juan A. Garcia Pardo <juagargi@gmail.com>
2021-07-28 15:40:16 +0200

    gofmt go/

--- go/pkg/storage/storage.go
+++ go/pkg/storage/storage.go
@@ -19,10 +19,10 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/scionproto/scion/go/cs/beacon"
-	sqlitebeacondb "github.com/scionproto/scion/go/cs/beacon/beacondbsqlite"
 	sqlitereservation "github.com/scionproto/scion/go/co/reservation/sqlite"
 	"github.com/scionproto/scion/go/co/reservationstorage/backend"
+	"github.com/scionproto/scion/go/cs/beacon"
+	sqlitebeacondb "github.com/scionproto/scion/go/cs/beacon/beacondbsqlite"
 	"github.com/scionproto/scion/go/lib/addr"
 	"github.com/scionproto/scion/go/lib/config"
 	"github.com/scionproto/scion/go/lib/drkey"
