6ac27a0a17426fa0f860ab0345314e20ad127f88
Juan A. Garcia Pardo <juagargi@gmail.com>
2021-07-28 15:38:41 +0200

    make mocks

--- go/co/reservationstorage/backend/mock_backend/mock.go
+++ go/co/reservationstorage/backend/mock_backend/mock.go
@@ -112,6 +112,21 @@ func (mr *MockDBMockRecorder) DeleteSegmentRsv(arg0, arg1 interface{}) *gomock.C
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeleteSegmentRsv", reflect.TypeOf((*MockDB)(nil).DeleteSegmentRsv), arg0, arg1)
 }
 
+// GetAllE2ERsvs mocks base method
+func (m *MockDB) GetAllE2ERsvs(arg0 context.Context) ([]*e2e.Reservation, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetAllE2ERsvs", arg0)
+	ret0, _ := ret[0].([]*e2e.Reservation)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetAllE2ERsvs indicates an expected call of GetAllE2ERsvs
+func (mr *MockDBMockRecorder) GetAllE2ERsvs(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetAllE2ERsvs", reflect.TypeOf((*MockDB)(nil).GetAllE2ERsvs), arg0)
+}
+
 // GetAllSegmentRsvs mocks base method
 func (m *MockDB) GetAllSegmentRsvs(arg0 context.Context) ([]*segment.Reservation, error) {
 	m.ctrl.T.Helper()
