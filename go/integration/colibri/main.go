bf24692de89ff3030f16488efb9ab24cedd7ce02
Juan A. Garcia Pardo <juagargi@gmail.com>
2021-09-23 16:19:09 +0200

    Add colibri checks to integration test.
    Create reservation, use it and clean it.
    The client needs to see the response using colibri.

--- go/integration/colibri/main.go
+++ go/integration/colibri/main.go
@@ -15,21 +15,27 @@
 package main
 
 import (
-	"bytes"
 	"context"
+	"crypto/rand"
 	"encoding/hex"
 	"flag"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/scionproto/scion/go/integration"
 	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/colibri"
+	libcol "github.com/scionproto/scion/go/lib/colibri"
+	"github.com/scionproto/scion/go/lib/colibri/reservation"
+	"github.com/scionproto/scion/go/lib/common"
 	libint "github.com/scionproto/scion/go/lib/integration"
 	"github.com/scionproto/scion/go/lib/log"
 	"github.com/scionproto/scion/go/lib/sciond"
 	"github.com/scionproto/scion/go/lib/serrors"
+	colpath "github.com/scionproto/scion/go/lib/slayers/path/colibri"
 	"github.com/scionproto/scion/go/lib/snet"
 	"github.com/scionproto/scion/go/lib/sock/reliable"
 	"github.com/scionproto/scion/go/lib/util"
@@ -92,11 +98,39 @@ func (s server) run() {
 		fmt.Printf("%s%s\n\n", libint.ReadySignal, integration.Local.IA)
 	}
 
+	go func() {
+		defer log.HandlePanic()
+		s.allowAdmission(integration.SDConn(), integration.Local.Host.IP)
+	}()
 	for {
 		buffer := make([]byte, 16384)
 		if err := s.accept(conn, buffer); err != nil {
 			log.Error("accepting connection", "err", err)
 		}
+	}
+}
+
+func (s server) allowAdmission(daemon sciond.Connector, serverIP net.IP) {
+	for {
+		ctx, cancelF := context.WithTimeout(context.Background(), 3*time.Second)
+		entry := &colibri.AdmissionEntry{
+			DstHost:         serverIP, // could be empty to detect it automatically
+			ValidUntil:      time.Now().Add(time.Minute),
+			RegexpIA:        "", // from any AS
+			RegexpHost:      "", // from any host
+			AcceptAdmission: true,
+		}
+		log.Debug("server, adding admission entry", "ip", serverIP)
+		validUntil, err := daemon.ColibriAddAdmissionEntry(ctx, entry)
+		if err != nil {
+			integration.LogFatal("establishing admission from server", "err", err)
+		}
+		if time.Until(validUntil).Seconds() < 45 {
+			integration.LogFatal("too short validity, something went wrong",
+				"requested", entry.ValidUntil, "got", validUntil)
+		}
+		cancelF()
+		time.Sleep(30 * time.Second)
 	}
 }
 
@@ -111,11 +145,18 @@ func (s server) accept(conn *snet.Conn, buffer []byte) error {
 	}
 
 	data := buffer[:n]
-	if !bytes.Equal(data, ([]byte)("test test")) {
+	if !strings.HasPrefix(string(data), "colibri test") {
 		return serrors.New("unknown received pattern", "pattern", string(data),
 			"hex", hex.EncodeToString(data))
 	}
-	log.Info("received pattern", "sender", fromScion.String())
+	log.Info("received pattern", "sender", fromScion.String(), "str", string(data))
+	n2, err := conn.WriteTo(data, from)
+	if err != nil {
+		return serrors.WrapStr("writing echo response from server", err)
+	}
+	if n2 != len(data) {
+		return serrors.New("wrong size writing", "data_len", len(data), "written", n2)
+	}
 	return nil
 }
 
@@ -127,5 +168,117 @@ func (c client) run(remote *snet.UDPAddr) int {
 	defer log.Info("Finished", "pair", pair)
 	defer integration.Done(integration.Local.IA, remote.IA)
 
+	ctx, cancelF := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelF()
+	deadline, _ := ctx.Deadline()
+
+	// find a path to the destination
+	pathquerier := sciond.Querier{
+		Connector: integration.SDConn(),
+		IA:        integration.Local.IA,
+	}
+	pathsToDst, err := pathquerier.Query(ctx, remote.IA)
+	if err != nil {
+		integration.LogFatal("obtaining paths", "err", err)
+	}
+	if len(pathsToDst) == 0 {
+		integration.LogFatal("no paths found")
+	}
+	pathToDst := pathsToDst[0]
+	log.Debug("found path to destination", "path", pathToDst)
+	remote.Path = pathToDst.Path()
+	remote.NextHop = pathToDst.UnderlayNextHop()
+	// dial to destination using the first path
+	dispatcher := reliable.NewDispatcher(reliable.DefaultDispPath)
+	scionNet := snet.NewNetwork(integration.Local.IA, dispatcher, sciond.RevHandler{
+		Connector: integration.SDConn()})
+	log.Debug("dialing with best effort", "addr", remote.String(), "path", remote.Path)
+	conn, err := scionNet.Dial(ctx, "udp", integration.Local.Host, remote, addr.SvcNone)
+	if err != nil {
+		integration.LogFatal("dialing", "err", err)
+	}
+	err = conn.SetDeadline(deadline)
+	if err != nil {
+		integration.LogFatal("setting deadline", "err", err)
+	}
+	_, err = conn.WriteTo([]byte("colibri test best effort"), remote)
+	if err != nil {
+		integration.LogFatal("writing data with best effort", "err", err)
+	}
+	// read echo back
+	buff := make([]byte, 1500)
+	_, err = conn.Read(buff)
+	if err != nil {
+		integration.LogFatal("reading data", "err", err)
+	}
+	stitchable, err := c.listRsvs(ctx, integration.SDConn(), remote.IA)
+	if err != nil {
+		integration.LogFatal("listing reservations", "err", err)
+	}
+	log.Debug("listed reservations", "list", stitchable.String())
+	trips := libcol.CombineAll(stitchable)
+	log.Info("computed full trips", "count", len(trips))
+	if len(trips) == 0 {
+		integration.LogFatal("no trips found")
+	}
+	// obtain an reservation
+	p, err := c.createRsv(ctx, integration.SDConn(), integration.Local.IA, remote, trips[0].Segments(), 1)
+	if err != nil {
+		integration.LogFatal("creating reservation", "err", err)
+	}
+	// use the reservation
+	remote.Path = p.Path()
+	remote.NextHop = p.UnderlayNextHop()
+	_, err = conn.WriteTo([]byte("colibri test colibri path"), remote)
+	if err != nil {
+		integration.LogFatal("writing data with colibri", "err", err)
+	}
+	// read echo back again
+	_, raddr, err := conn.ReadFrom(buff)
+	if err != nil {
+		integration.LogFatal("reading data", "err", err)
+	}
+	sraddr, ok := raddr.(*snet.UDPAddr)
+	if !ok {
+		integration.LogFatal("sender of response is not scion", "raddr", raddr,
+			"type", common.TypeOf(raddr))
+	}
+	if sraddr.Path.Type != colpath.PathType {
+		integration.LogFatal("non-colibri path type", "type", sraddr.Path.Type)
+	}
 	return 0
+}
+
+func (c client) listRsvs(ctx context.Context, daemon sciond.Connector, remote addr.IA) (
+	*libcol.StitchableSegments, error) {
+	for {
+		stitchable, err := daemon.ColibriListRsvs(ctx, remote)
+		if err != nil {
+			return nil, err
+		}
+		if stitchable != nil {
+			return stitchable, nil
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func (c client) createRsv(ctx context.Context, daemon sciond.Connector, localIA addr.IA,
+	remoteAddr *snet.UDPAddr, segments []reservation.ID, requestBW reservation.BWCls) (
+	snet.Path, error) {
+
+	setupReq := &libcol.E2EReservationSetup{
+		Id: reservation.ID{
+			ASID:   localIA.A,
+			Suffix: make([]byte, 12),
+		},
+		SrcIA:       localIA,
+		DstIA:       remoteAddr.IA,
+		DstHost:     remoteAddr.Host.IP,
+		Index:       0, // new index
+		Segments:    segments,
+		RequestedBW: requestBW,
+	}
+	rand.Read(setupReq.Id.Suffix) // random suffix
+	return daemon.ColibriSetupRsv(ctx, setupReq)
 }
