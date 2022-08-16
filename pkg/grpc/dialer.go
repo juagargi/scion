f8a3115c25aa8ec8c4b220048b5d7ef233c44c4a
Juan A. Garcia Pardo <juagargi@gmail.com>
2022-08-16 12:42:09 +0200

    grpc: fix context canceled on repeated dial.
    The dialer closure that acts as a gRPC dialer in WithContextDialer
    needs to use the ctx Context argument and not capture the
    ctx variable from the parent function.

--- pkg/grpc/dialer.go
+++ pkg/grpc/dialer.go
@@ -151,7 +151,7 @@ func (d *QUICDialer) Dial(ctx context.Context, addr net.Addr) (*grpc.ClientConn,
 		return nil, serrors.New("wrong address type after svc resolution",
 			"type", common.TypeOf(addr))
 	}
-	dialer := func(context.Context, string) (net.Conn, error) {
+	dialer := func(ctx context.Context, _ string) (net.Conn, error) {
 		return d.Dialer.Dial(ctx, addr)
 	}
 	return grpc.DialContext(ctx, addr.String(),
@@ -180,7 +180,7 @@ func (d *TLSQUICDialer) Dial(ctx context.Context, addr net.Addr) (*grpc.ClientCo
 		return nil, serrors.New("wrong address type after svc resolution",
 			"type", common.TypeOf(addr))
 	}
-	dialer := func(context.Context, string) (net.Conn, error) {
+	dialer := func(ctx context.Context, _ string) (net.Conn, error) {
 		return d.Dialer.Dial(ctx, addr)
 	}
 	return grpc.DialContext(ctx, addr.String(),
