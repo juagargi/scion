6d7bc8020d7e767b839fef727d79a7857c47c58e
Juan A. Garcia Pardo <juagargi@gmail.com>
2025-03-14 15:31:54 +0100

    Alias services.

--- control/aliases/grpc/server.go
+++ control/aliases/grpc/server.go
@@ -17,25 +17,65 @@ package grpc
 import (
 	"context"
 
+	"github.com/scionproto/scion/pkg/addr"
+	"github.com/scionproto/scion/pkg/alias"
+	libgrpc "github.com/scionproto/scion/pkg/grpc"
+	"github.com/scionproto/scion/pkg/private/util"
 	cppb "github.com/scionproto/scion/pkg/proto/control_plane"
+	"github.com/scionproto/scion/private/aliasdb"
 )
 
-type AliasesServer struct{}
+type AliasesServer struct {
+	DB     aliasdb.DB
+	Dialer libgrpc.Dialer // To request other ASes' control services.
+}
 
-func NewAliasesServer() *AliasesServer {
-	return nil
+func NewAliasesServer(
+	db aliasdb.DB,
+	dialer libgrpc.Dialer,
+) *AliasesServer {
+	return &AliasesServer{
+		DB:     db,
+		Dialer: dialer,
+	}
 }
 
 func (s *AliasesServer) RegisterAlias(
 	ctx context.Context,
 	req *cppb.RegisterAliasRequest,
 ) (*cppb.RegisterAliasResponse, error) {
-	return nil, nil
+	// Register in local DB.
+	replicas := make([]alias.Replica, 0, len(req.Replicas))
+	for _, rep := range req.Replicas {
+		replicas = append(replicas, alias.Replica{
+			IA:       addr.IA(rep.IsdAs),
+			Hostname: rep.Host,
+			NotAfter: util.SecsToTime(rep.NotAfter),
+		})
+	}
+
+	err := s.DB.AddReplicas(ctx, req.Host, replicas)
+	return &cppb.RegisterAliasResponse{}, err
 }
 
 func (s *AliasesServer) GetAliases(
 	ctx context.Context,
 	req *cppb.GetAliasesRequest,
 ) (*cppb.GetAliasesResponse, error) {
-	return nil, nil
+	// Return local database entries.
+	replicas, err := s.DB.Get(ctx, req.Host)
+	reps := make([]*cppb.Replica, 0)
+	for _, rep := range replicas {
+		reps = append(reps, &cppb.Replica{
+			IsdAs:    uint64(rep.IA),
+			Host:     rep.Hostname,
+			NotAfter: util.TimeToSecs(rep.NotAfter),
+		})
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &cppb.GetAliasesResponse{
+		Replicas: reps,
+	}, nil
 }
