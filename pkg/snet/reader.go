684ec211d00d09228337dd83678e4cf46720be76
Juan A. Garcia Pardo <juagargi@gmail.com>
2025-09-03 10:45:12 +0200

    Multihomed behavior through UDP socket creation.
    Via the creation of a UDP socket with destination the next hop,
    those snet sockets that were created without binding them to a local
    address, can now be used regularly.
    There is a additional cache that allows skipping the UDP socket creation
    if the address to dial to has been previously seen.
    There is a recurrent refresh function running in the background that
    checks that the host interfaces' local addresses didn't change, and if
    they did, clears the cache.

--- pkg/snet/reader.go
+++ pkg/snet/reader.go
@@ -16,7 +16,6 @@ package snet
 
 import (
 	"net"
-	"net/netip"
 	"sync"
 	"time"
 
@@ -86,21 +85,6 @@ func (c *scionConnReader) read(b []byte) (int, *UDPAddr, error) {
 	udp, ok := pkt.Payload.(UDPPayload)
 	if !ok {
 		return 0, nil, serrors.New("unexpected payload", "type", common.TypeOf(pkt.Payload))
-	}
-
-	// XXX(JordiSubira): We explicitly forbid nil or unspecified address in the current constructor
-	// for Conn.
-	// If this were ever to change, we would always fall into the following if statement, then
-	// we would like to replace this logic (e.g., using IP_PKTINFO, with its caveats).
-	pktAddrPort := netip.AddrPortFrom(pkt.Destination.Host.IP(), udp.DstPort)
-	if c.local.IA != pkt.Destination.IA ||
-		c.local.Host.AddrPort() != pktAddrPort {
-		return 0, nil, serrors.New("packet is destined to a different host",
-			"local_isd_as", c.local.IA,
-			"local_host", c.local.Host,
-			"pkt_destination_isd_as", pkt.Destination.IA,
-			"pkt_destination_host", pktAddrPort,
-		)
 	}
 
 	// Extract remote address.
--- pkg/snet/writer.go
+++ pkg/snet/writer.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/scionproto/scion/pkg/addr"
 	"github.com/scionproto/scion/pkg/private/serrors"
+	"github.com/scionproto/scion/pkg/snet/multihomed"
 	"github.com/scionproto/scion/private/topology"
 )
 
@@ -81,6 +82,16 @@ func (c *scionConnWriter) WriteTo(b []byte, raddr net.Addr) (int, error) {
 	listenHostIP, ok := netip.AddrFromSlice(c.local.Host.IP)
 	if !ok {
 		return 0, serrors.New("invalid listen host IP", "ip", c.local.Host.IP)
+	}
+	if listenHostIP.IsUnspecified() {
+		// Sending data to an unbound socket, we need to find the appropriate local address
+		// to write it as host address in the SCION packet. The interface to use in the local
+		// host will be that one routed to reach the next hop.
+		localIP, err := multihomed.OutboundIP(nextHop)
+		if err != nil {
+			return 0, serrors.Wrap("interface IP not bound and cannot find one", err)
+		}
+		listenHostIP, _ = netip.AddrFromSlice(localIP)
 	}
 
 	pkt := &Packet{
