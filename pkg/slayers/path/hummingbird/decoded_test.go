7afa00bb3d0278c6b325912263c03f7ec81fdd7f
Juan A. Garcia Pardo <juagargi@gmail.com>
2026-07-08 13:29:12 +0200

    Test dataplane with explicit scion path reversed humm packet.

--- pkg/slayers/path/hummingbird/decoded_test.go
+++ pkg/slayers/path/hummingbird/decoded_test.go
@@ -19,7 +19,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
+	"github.com/scionproto/scion/pkg/scrypto"
 	"github.com/scionproto/scion/pkg/slayers/path"
 	"github.com/scionproto/scion/pkg/slayers/path/hummingbird"
 	"github.com/scionproto/scion/pkg/slayers/path/scion"
@@ -52,41 +54,107 @@ func TestSerializeAndBack(t *testing.T) {
 }
 
 func TestDecodedDecodeFromBytesNoFlyovers(t *testing.T) {
+	const hfExpTime = 8
+	const upTimestamp = 0x01020304
+	const downTimestamp = 0x05060708
+
+	macKey := []byte("testkey_xxxxxxxx")
 	// p is the scion decoded path we would observe using the Tiny topology of the
 	// topology generator, when going from 111 to 112. This is one up segment with 2 hops, followed
 	// by a down segment with two hops as well. There is a cross over at core 110 gluing both.
 	p := &scion.Decoded{
 		Base: scion.Base{
 			PathMeta: scion.MetaHdr{
-				SegLen: [3]uint8{2, 2, 0},
+				CurrINF: 1,
+				CurrHF:  2,
+				SegLen:  [3]uint8{2, 2, 0},
 			},
 			NumINF:  2,
 			NumHops: 4,
 		},
 		InfoFields: []path.InfoField{
-			{}, // up
-			{}, // down
+			{
+				ConsDir:   false, // up
+				SegID:     0x111,
+				Timestamp: upTimestamp,
+			},
+			{
+				ConsDir:   true, // down
+				SegID:     0x222,
+				Timestamp: downTimestamp,
+			},
 		},
 		HopFields: []path.HopField{
-			{}, // 111: 0->41 up
-			{}, // 110: 1->0  up
-			{}, // 110: 0->2  down
-			{}, // 112: 1->0  down
+			{
+				ConsIngress: 41, // 111: 0->41 up
+				ConsEgress:  0,
+				ExpTime:     hfExpTime,
+			},
+			{
+				ConsIngress: 0, // 110: 1->0 up
+				ConsEgress:  1,
+				ExpTime:     hfExpTime,
+			},
+			{
+				ConsIngress: 0, // 110: 0->2 down
+				ConsEgress:  2,
+				ExpTime:     hfExpTime,
+			},
+			{
+				ConsIngress: 1, // 112: 1->0 down
+				ConsEgress:  0,
+				ExpTime:     hfExpTime,
+			},
 		},
 	}
+	p.HopFields[0].Mac = computeHopMAC(t, macKey, p.InfoFields[0], p.HopFields[0])
+	p.HopFields[1].Mac = computeHopMAC(t, macKey, p.InfoFields[0], p.HopFields[1])
+	p.HopFields[2].Mac = computeHopMAC(t, macKey, p.InfoFields[1], p.HopFields[2])
+	p.HopFields[3].Mac = computeHopMAC(t, macKey, p.InfoFields[1], p.HopFields[3])
 
 	// Create a hummingbird path from the scion one.
 	hbird := &hummingbird.Decoded{}
 	hbird.ConvertFromScionDecoded(p) // SegLen will be [6,6,0] after this
 
+	expected := &hummingbird.Decoded{
+		Base: hummingbird.Base{
+			PathMeta: hummingbird.MetaHdr{
+				CurrINF: 1,
+				CurrHF:  6,
+				SegLen:  [3]uint8{6, 6, 0},
+			},
+			NumINF:   2,
+			NumLines: 12,
+		},
+		InfoFields: p.InfoFields,
+		HopFields: []hummingbird.FlyoverHopField{
+			{HopField: p.HopFields[0]},
+			{HopField: p.HopFields[1]},
+			{HopField: p.HopFields[2]},
+			{HopField: p.HopFields[3]},
+		},
+		FirstHopPerSeg: [2]uint8{2, 4},
+	}
+	assert.Equal(t, expected, hbird)
+
 	// Check the hummingbird path is correct by serializing and deserializing it.
 	buf := make([]byte, hbird.Len())
 	err := hbird.SerializeTo(buf)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	// Deserialize.
 	hbird = &hummingbird.Decoded{}
 	err = hbird.DecodeFromBytes(buf)
-	assert.NoError(t, err)
+	require.NoError(t, err)
+	assert.Equal(t, expected, hbird)
+}
+
+func computeHopMAC(t *testing.T, key []byte, info path.InfoField,
+	hf path.HopField) [path.MacLen]byte {
+
+	t.Helper()
+	mac, err := scrypto.InitMac(key)
+	require.NoError(t, err)
+	return path.MAC(mac, info, hf, nil)
 }
 
 func TestDecodedReverseHbird(t *testing.T) {
